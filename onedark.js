7688f9244e01f9d38eb404eb6bbeb6bf176e9bfe
ohjoonhee <65221542+ohjoonhee@users.noreply.github.com>
2023-03-19 21:38:33 +0900

    Add files via upload

---
+++ onedark.js
@@ -0,0 +1,27 @@
+black = '#3f4451';
+red = '#e05561'; // red
+green = '#8cc265'; // green
+yellow = '#d18f52'; // yellow
+blue = '#4aa5f0'; // blue
+magenta = '#c162de'; // pink
+cyan = '#42b3c2'; // cyan
+white = '#d7dae0'; // light gray
+lightBlack = '#4f5666'; // medium gray
+lightRed = '#ff616e'; // red
+lightGreen = '#a5e075'; // green
+lightYellow = '#f0a45d'; // yellow
+lightBlue = '#4dc4ff'; // blue
+lightMagenta = '#de73ff'; // pink
+lightCyan = '#4cd1e0'; // cyan
+lightWhite = '#e6e6e6'; // white
+
+t.prefs_.set('color-palette-overrides',
+    [black, red, green, yellow,
+        blue, magenta, cyan, white,
+        lightBlack, lightRed, lightGreen, lightYellow,
+        lightBlue, lightMagenta, lightCyan, lightWhite
+    ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#000000');
+t.prefs_.set('background-color', white);
